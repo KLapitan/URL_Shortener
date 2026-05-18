9cb65f09554b446de596e5b67451a80c046d8428
KLapitan <karllapitan5@gmail.com>
2026-05-18 16:30:40 +0800

    done:Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,19 +1,20 @@
  const Footer = ({links,socMedia}) => {
  return (
  <footer className="h-auto">
-    <section className="h-auto w-full bg-NGray950 flex flex-col justify-center items-center font-Poppins ">
+    <section className="h-auto w-full bg-NGray950 flex flex-col justify-center items-center px-6 py-12 font-Poppins lg:flex-row lg:items-start lg:justify-evenly  ">
         
-          <picture className="border w-45 m-10" >
+          <picture className="border w-45  " >
             <img src="/images/logo.svg" alt="logoShorlty"  className="invert brightness-0 w-full h-full"/>
           </picture>
         
-        <ul className="flex flex-col gap-7 border mb-9 ">
+        <ul className="flex flex-col gap-10 items-center lg:flex-row lg:items-start mt-10 lg:mt-0">
           {links.map((link) => (
-          <li key={link.id} className="text-white text-xl text-center font-bold ">{link.linkName}
+          <li key={link.id}  className="text-white text-xl font-bold text-center lg:text-left">{link.linkName}
           {link.sublinks && link.sublinks.length > 0 && (
-          <ul className="flex flex-col gap-2 m-2 mt-7"> 
+
+          <ul className="flex flex-col gap-2 mt-6"> 
             {link.sublinks.map((item,i) => (
-            <li key={i} className="text-NGray500 text-md font-normal">{item}</li>
+            <li key={i} className="text-NGray500 text-md font-normal lg:text-left " >{item}</li>
             ))}
           </ul>
           
@@ -26,11 +27,11 @@
         </ul>
 
 
-        <ul className="flex flex-row gap-10 mb-12">
+        <ul className="flex gap-8 mt-10 lg:mt-0">
         {socMedia.map((link) => (
         <li >
           <picture>
-            <img src={link.src} alt={link.alt}  className="w-full h-full scale-150"/>
+            <img src={link.src} alt={link.alt}  className="scale-125"/>
           </picture>
         </li>
         ))}
