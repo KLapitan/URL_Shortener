bcc418d4b60277de79ba942e76f09cefedc0c765
KLapitan <karllapitan5@gmail.com>
2026-05-19 14:25:51 +0800

    edit:input bar

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -2,11 +2,11 @@ import Button from "./button"
 const UrlInput = () => {
 return(
 <section className="h-75 relative z-20">
-<div className=" h-35 w-full bg-white z-30 ">
+<div className=" h-35 w-full bg-white z-30 flex justify-center items-center">
 
 
 {/* input */}
-<div className="absolute min-w-auto sm:w-full sm:left-0 border h-38 bg-NGray900 z-70 top-15 left-5 p-4 rounded-lg flex flex-col gap-4 font-Poppins bg-[url('/images/bg-shorten-mobile.svg')] bg-[length:80%] bg-top-right bg-no-repeat">
+<div className="absolute min-w-auto sm:w-full sm:left-0 border h-38 bg-NGray900 z-70 top-15   p-4 rounded-lg flex flex-col gap-4 font-Poppins bg-[url('/images/bg-shorten-mobile.svg')] bg-[length:80%] bg-top-right bg-no-repeat">
 <input type="text" placeholder="Shorten a link here.."  className="bg-white w-full h-12 rounded-md px-4"/>
 <Button classname="bg-PBlue400 w-full h-12 rounded-md text-white font-bold ">Shorten it!</Button>
 </div>
