5cc701d3dc69d380bed93d49e195d2f463e724cb
KLapitan <karllapitan5@gmail.com>
2026-05-26 14:59:07 +0800

    fix: text title of hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -8,21 +8,21 @@ return(
 <main className="h-auto">
     <section className="h-auto" >
       {/* mid - large view flex row reverse */}
-      <div className="flex flex-col sm:flex-row-reverse max-w-auto   sm:h-90 lg:h-126 2xl:h-130">
+      <div className="flex flex-col sm:flex-row-reverse    sm:h-90 lg:h-126 2xl:h-130">
 
         {/* image container */}
-          <div className="max-w-2xl  h-80   overflow-hidden  sm:h-full   2xl:w-2xl ">
+          <div className=" max-w-2xl  h-80   overflow-hidden  sm:h-full   2xl:w-2xl   ">
               <picture>
-                <img src="/images/illustration-working.svg" alt="herosectionlogo"  className=" w-full h-full  object-cover object-left ml-7 lg:object-contain lg:overflow-hidden lg:ml-20 lg:-mt-3 lg:scale-100" />
+                <img src="/images/illustration-working.svg" alt="herosectionlogo"  className=" w-full h-full  object-cover object-left ml-7 lg:object-contain lg:overflow-hidden lg:ml-20 lg:-mt-3 lg:scale-120 xl:scale-100  2xl:scale-112 2xl:ml-10 " />
               </picture>
             
           </div>
-            <div className="max-w-2xl font-Poppins p-4 flex flex-col items-center  gap-4  sm:items-start sm:justify-center  lg:w-xl 2xl:w-2xl">
-                <h2 className="text-center text-4xl font-bold tracking-tight sm:text-left lg:text-7xl lg:-ml-14 2xl:-ml-8">More than just shorter links</h2>
+            <div className="max-w-2xl lg:mx-1    xl:w-xl font-Poppins sm:p-2 flex flex-col items-center  gap-4  sm:items-start  sm:justify-center  lg:p-2 lg:w-152   2xl:w-2xl   ">
+                <h2 className="text-center text-4xl font-bold tracking-tight sm:text-left 2md:text-xl lg:text-6xl lg:text-left xl:-ml-2 2xl:text-7xl 2xl:-ml-3">More than just shorter links</h2>
 
-                <p className="text-center text-NGray500 w-72 sm:text-left lg:w-140 lg:text-xl lg:-ml-14 2xl:-ml-8">Build your brand’s recognition and get detailed insights on how your links are performing.</p>
+                <p className="text-center text-NGray500 w-72 sm:text-left lg:w-140 lg:text-lg lg:text-left xl:-ml-2 2xl:-ml-3">Build your brand’s recognition and get detailed insights on how your links are performing.</p>
 
-                <Button className="text-white bg-PBlue400 font-bold text-xl  py-4 rounded-full w-46 h-full sm:h-15 lg:h-12 lg:py-2 lg:w-42 lg:text-sm lg:-ml-14 2xl:-ml-8">Get Started</Button>
+                <Button className="text-white bg-PBlue400 font-bold text-xl  py-4 rounded-full w-46 h-full sm:h-15 lg:h-12 lg:py-2 lg:w-42 lg:text-sm  xl:-ml-2 2xl:-ml-3">Get Started</Button>
             </div>
           </div>
 
